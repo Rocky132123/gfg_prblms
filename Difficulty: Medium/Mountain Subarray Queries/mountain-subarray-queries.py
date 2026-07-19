f918de1f43fed8816b3b9fbb6a4d1a85f46f27af
Kalluri Rakesh <kallurirakesh9@gmail.com>
2026-07-19 22:04:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Mountain Subarray Queries/mountain-subarray-queries.py
@@ -0,0 +1,27 @@
+class Solution:
+    def processQueries(self, arr, queries):
+        n = len(arr)
+
+        up = [0] * n
+        down = [0] * n
+
+        up[n - 1] = n - 1
+        for i in range(n - 2, -1, -1):
+            if arr[i] <= arr[i + 1]:
+                up[i] = up[i + 1]
+            else:
+                up[i] = i
+
+        down[n - 1] = n - 1
+        for i in range(n - 2, -1, -1):
+            if arr[i] >= arr[i + 1]:
+                down[i] = down[i + 1]
+            else:
+                down[i] = i
+
+        ans = []
+        for l, r in queries:
+            peak = min(up[l], r)
+            ans.append(down[peak] >= r)
+
+        return ans
